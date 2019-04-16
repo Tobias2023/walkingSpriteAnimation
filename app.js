aaabfdcc55351614ad7563a0208625338f6f81f6
tscott2023 <38087336+Tobias2023@users.noreply.github.com>
2019-04-16 17:39:46 -0400

    first commit

---
+++ app.js
@@ -0,0 +1,7 @@
+function startWalk() {
+	document.getElementById("pumpkin").style.animationPlayState="running"
+}
+
+function stopWalk() {
+	document.getElementById("pumpkin").style.animationPlayState="paused"
+}
